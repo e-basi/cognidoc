633fc6d6c427d4e1b80da9474c50773bba321fca
Nicanor Obasi <nobasi@ryerson.ca>
2024-11-13 00:18:49 -0500

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,28 @@ import { UserButton, auth } from "@clerk/nextjs";
 export default async function Home() {
   const { userId } = await auth();
   const isAuth = !!userId;
+
   return (
     <div className="w-screen min-h-screen bg-gradient-to-tr from-blue-700 via-blue-800 to-gray-900">
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
-          <div className="flex items-center">
-            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+          <div className="flex items-center mb-4">
+            <h1 className="mr-3 text-5xl font-semibold text-white">Chat with any PDF</h1>
             <UserButton afterSignOutUrl="/" />
           </div>
 
-          <p className="max-w-xl mt-1 text-lg text-black">
-            Join millions of students, researchers and professionals to instantly
-            answer questions and understand research with AI
+          <p className="max-w-xl mt-1 text-lg text-gray-200">
+            Join millions of students, researchers, and professionals to instantly
+            answer questions and understand research with AI.
           </p>
 
-          <div className="flex mt-2"> {isAuth &&<button> Go to Chats</button>}</div>
-
+          {isAuth && (
+            <div className="mt-4">
+              <Button className="px-4 py-2 text-lg font-medium" href="/chats">
+                Go to Chats
+              </Button>
+            </div>
+          )}
         </div>
       </div>
     </div>
