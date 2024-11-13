1c861cc5f352bffab81024f4f108607f1a501bad
Nicanor Obasi <nicanorobasi@Nicanors-MacBook-Air.local>
2024-11-12 23:57:11 -0500

    uploaded

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
-import { Button } from  "@/components/ui/button"; 
+import { Button } from "@/components/ui/button";
+import { UserButton, auth } from "@clerk/nextjs";
 
 export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
   return (
-  <div className= "w-screen min-h-screen bg-gradient-to-tr from-blue-700 via-blue-800 to-gray-900"> 
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"> </div> </div>
+    <div className="w-screen min-h-screen bg-gradient-to-tr from-blue-700 via-blue-800 to-gray-900">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
 
+          <p className="max-w-xl mt-1 text-lg text-black">
+            Join millions of students, researchers and professionals to instantly
+            answer questions and understand research with AI
+          </p>
+
+          <div className="flex mt-2"> {isAuth &&<button> Go to Chats</button>}</div>
+
+        </div>
+      </div>
+    </div>
   );
-
 }
