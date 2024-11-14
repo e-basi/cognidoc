9724f5dd404261b51ba65818ff83cc81b13f3f70
Nicanor Obasi <nobasi@ryerson.ca>
2024-11-14 00:18:23 -0500

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import { Button } from "@/components/ui/button";
-import { UserButton, auth } from "@clerk/nextjs";
+import { UserButton} from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { Link, LogIn } from "lucide-react";
 
 export default async function Home() {
-  const { userId } = await auth();
+  const { userId, redirectToSignIn  } = await auth();
   const isAuth = !!userId;
+  
+  // Redirect to sign-in if not authenticated
+  if (!isAuth) {
+    return redirectToSignIn();
+  }
 
   return (
     <div className="w-screen min-h-screen bg-gradient-to-tr from-blue-700 via-blue-800 to-gray-900">
@@ -14,20 +21,32 @@ export default async function Home() {
             <UserButton afterSignOutUrl="/" />
           </div>
 
+          {isAuth && (
+            <div className="flex mt-3">
+              <Button className="px-4 py-2 text-lg font-medium">
+                Go to Chats
+              </Button>
+            </div>
+          )}
+  
           <p className="max-w-xl mt-1 text-lg text-gray-200">
             Join millions of students, researchers, and professionals to instantly
             answer questions and understand research with AI.
           </p>
 
-          {isAuth && (
-            <div className="mt-4">
-              <Button className="px-4 py-2 text-lg font-medium" href="/chats">
-                Go to Chats
-              </Button>
+
+          <div className="flex mt-4">
+            {isAuth ? ( <h1> fileUpload </h1> ):(
+              <link href="/sign-in">
+                 <Button>  Login to get Started! </Button>
+                    <LogIn className="w-4 h-4 m1-2" />
+                  </link>
+                  )}
             </div>
-          )}
+
+  
         </div>
       </div>
     </div>
   );
-}
+}  
