9b2543d0b9a7f62c85c5e508543044575da8bd04
Nicanor Obasi <nobasi@ryerson.ca>
2024-11-27 23:46:15 -0500

    updated schema.ts

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,4 +1,24 @@
-import {pgTable} from 'drizzle-orm/pg-core'
+import {PgTable, pgTable, serial, varchar, text, timestamp, pgEnum} from 'drizzle-orm/pg-core'
 
 
-export const chats =pgTable
+export const userSystemEnum = pgEnum('user_system_enum', ['system', 'user']);
+
+
+export const chats = pgTable('chats' , {
+    id: serial('id').primaryKey(),
+    pdfName: text('pdf_name').notNull(),
+    pdfUrl: text('pdf_url').notNull(),
+    crearedAt: timestamp('pdf_url').notNull().defaultNow(),
+    userId: varchar('user_id', {length:256}).notNull(),
+    fileKey: text('filekey').notNull(),
+
+})
+
+
+export const messages = pgTable("messages", {
+    id: serial('id').primaryKey(),
+    chatId: text('chat_id').references(()=>chats.id).notNull(),
+    pdfUrl: text('pdf_url').notNull(),
+    crearedAt: timestamp('pdf_url').notNull().defaultNow(),
+    role: userSystemEnum("role").notNull(),
+})
