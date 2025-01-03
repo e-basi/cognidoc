67ef508a28024b623ca0c0195e11fe6e70735bc7
e-basi <nobasi@ryerson.ca>
2025-01-02 23:48:24 -0500

    updated pinecone.ts

--- src/lib/db/pinecone.ts
+++ src/lib/db/pinecone.ts
@@ -1,11 +1,20 @@
 // pinecone.ts
 import { Pinecone } from '@pinecone-database/pinecone';
+import { downloadFromS3 } from "./s3-server";
 
 const pinecone = new Pinecone({
+  // environment: process.env.PINECONE_ENVIRONMENT!, // e.g., "us-east1-gcp"
   apiKey: process.env.PINECONE_API_KEY!,
 });
 
 export default pinecone;
 
 
-export async function loadS3IntoPinecone(fileKey: string){}
+export async function loadS3IntoPinecone(fileKey: string) {
+  // Add your implementation here
+  console.log("downloading s3 into file system");
+  const file_name = await downloadFromS3(fileKey);
+  if (!file_name) {
+    throw new Error("could not download from s3");
+  }
+}
